4c52a6408bf43a66881135787e824cec70f4f753
mathieuduperre <mathieu.duperre@gmail.com>
2017-09-20 09:26:45 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
-## linuxgsm-docker base image entrypoint script
-## execute linuxgsm or arbitrary server commands at will
-## by passing command
 
-
-## Because of a limitation in LGSM script it must be run from the directory
-## It is installed in.
-##
-## If one wants to use a volume for the data directory, which is the home directory
-## then we must keep a backup copy of the script on local drive
 if [ ! -e ~/linuxgsm ]; then
     echo "Initializing Linuxgsm User Script in New Volume"
     cp /linuxgsm.sh ./linuxgsm
 fi
 
-# with no command, just spawn a running container suitable for exec's
+# if an attribute is added (name of the game), overwrite env variable GAME_NAME
 if [ $# = 1 ]; then
     export GAME_NAME=$@
+    echo "Game to be installed:"
+    echo $GAME_NAME
 fi
 
 ./linuxgsm $GAME_NAME
-$@ auto-install
-$@ start
+$GAME_NAME auto-install
+$GAME_NAME start
 while true; do sleep 1000; done
-
 
 exit 0
